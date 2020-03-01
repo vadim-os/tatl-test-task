cc7786efe32888b25335245598db6d389064bd21
vadim-os <osinsky_vadim@yahoo.com>
2020-03-01 17:41:54 +0200

    Updates

---
+++ precache-manifest.e00a8ad223e1f86cbba984869a3c5de5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010fa9e74113ff5fddc46aa2bcb011dc",
+    "url": "/tatl-test-task/index.html"
+  },
+  {
+    "revision": "10ef825fed9694f02aec",
+    "url": "/tatl-test-task/static/css/main.0e7564fd.chunk.css"
+  },
+  {
+    "revision": "a106f01112dff8367de2",
+    "url": "/tatl-test-task/static/js/2.73ded539.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tatl-test-task/static/js/2.73ded539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10ef825fed9694f02aec",
+    "url": "/tatl-test-task/static/js/main.cfd50b1f.chunk.js"
+  },
+  {
+    "revision": "77140bbdb65722b7e0f8",
+    "url": "/tatl-test-task/static/js/runtime-main.17fb20bc.js"
+  }
+]);
